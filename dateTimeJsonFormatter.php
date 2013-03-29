9096df86ae54e3084ea4b099f8b3531f9db4090e
Khatharsis <tchoonta@gmail.com>
2013-03-28 19:44:40 -0700

    Added clock functionality--time now displays seconds which should update every 1000ms (1 sec).

--- dateTimeJsonFormatter.php
+++ dateTimeJsonFormatter.php
@@ -1,12 +1,45 @@
 <?php
-    date_default_timezone_set('America/Los_Angeles'); 
-    $hereTime = date("H:i");
-    //echo "<br/>";
-    date_default_timezone_set('Asia/Bangkok');
-    $thereTime = date("H:i");
-    //echo "<br/>";
-    date_default_timezone_set('America/Los_Angeles');
+    //http://www.php.net/manual/en/function.getdate.php#87691
+    function gmGetDate($timestamp = null) {
+        $arr = array('seconds', 'minutes', 'hours', 'mday', 'wday', 'mon',
+        'year', 'yday', 'weekday', 'month', 0);
+        return (array_combine($arr, split(':', 
+            gmdate('s:i:G:j:w:n:Y:z:l:F:U', is_null($timestamp) ? time() :
+            $timestamp))));
+    }
 
+    // Only changes the hour offset; can be modified to adjust for
+    // date changes as well, but irrelevant in this script.
+    function adjustGmtDate($gmtDate, $timezoneOffset) {
+        $gmtDate["hours"] = $gmtDate["hours"] + $timezoneOffset;
+        if ($gmtDate["hours"] > 24) {
+            $gmtDate["hours"] = $gmtDate["hours"] - 24;
+        }
+        else if ($gmtDate["hours"] < 0) {
+            $gmtDate["hours"] = $gmtDate["hours"] + 24;
+        }
+        return $gmtDate;
+    }
+    
+    $gmtDate = gmGetDate();
+
+    // Here: LA, must account for daylight savings
+    $localT = localtime(time(), true);
+    $hereDate = null;
+    if ($localT['tm_isdst'] > 0) {
+        $hereDate = adjustGmtDate($gmtDate, -7);
+    }
+    else {
+        $hereDate = adjustGmtDate($gmtDate, -8);
+    }
+
+    // There: BKK, no daylight savings
+    $thereDate = adjustGmtDate($gmtDate, 7);
+
+    $hereHour = $hereDate['hours'];
+    $thereHour = $thereDate['hours'];
+    $minutes = $hereDate['minutes'];
+    
     $daysLeft = '';
     $currentDate = strtotime("now");
     $futureDate = strtotime("05/10/2013");
@@ -29,8 +62,14 @@
 echo <<<EOT
 {
     "currentTime": {
-        "here": "$hereTime",
-        "there": "$thereTime"
+        "here": "$hereHour:$minutes",
+        "here_h": "$hereHour",
+        "here_m": "$minutes",
+        "here_s": "0",
+        "there": "$thereHour:$minutes",
+        "there_h": "$thereHour",
+        "there_m": "$minutes",
+        "there_s": "0"
     },
     "daysLeft": {
         "remaining": "$daysLeft"
@@ -39,8 +78,12 @@ echo <<<EOT
 EOT;
 //{
 //    "currentTime": {
-//        "here": "8:19pm",
-//        "there": "10:19am"
+//        "here": "20:19",
+//        "here_h": "20",
+//        "here_m": "19",
+//        "there": "10:19"
+//        "there_h": "10",
+//        "there_m": "19"
 //    },
 //    "daysLeft": {
 //        "remaining": "8w 1d"
--- scripts/hereThere.js
+++ scripts/hereThere.js
@@ -1,28 +1,42 @@
 // Set up the Canvas
 function arrangeCanvas() {
-	// Using jQuery with Canvas isn't too straightfoward...
-	// http://stackoverflow.com/questions/3305167/simple-html5-canvas-and-jquery-question
     var weatherObj = {},
         timeObj = {};
 
+    var intervalId = 0;
+    // Separate canvases to reduce amount of drawing
+    // Background image canvas 
     var canvas = $('#app')[0],
         ctx = null,
         backgroundImage = new Image();
 
+    // Text canvas 
+    var canvasText = $('#app-text')[0],
+        ctx2 = null;
+	
+    // Using jQuery with Canvas isn't too straightfoward...
+	// http://stackoverflow.com/questions/3305167/simple-html5-canvas-and-jquery-question
 	if (canvas.getContext) {
 		ctx = canvas.getContext('2d');
+        ctx2 = canvasText.getContext('2d');
 	    
         backgroundImage.onload = function() {
             // Loading...
             ctx.drawImage(backgroundImage, 0, 0);
-            printLoadingMessage(ctx);
+            printLoadingMessage(ctx2);
 
             // After fetching data
             $.when(getWeather(weatherObj), getTime(timeObj))
             .done(function() {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(backgroundImage, 0, 0);
-                printObjects(ctx, weatherObj, timeObj);
+                ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
+                printObjects(ctx2, weatherObj, timeObj);
+
+                // Refresh text canvas every second
+                intervalId = setInterval(function() {
+                    updateTimeObj(timeObj, null);
+                    ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
+                    printObjects(ctx2, weatherObj, timeObj);
+                }, 1000);
             });
         };
         backgroundImage.src = 'images/tempWallpaper.jpg';
@@ -49,15 +63,23 @@ function getWeather(obj) {
 
 // Call custom PHP script to obtain time values
 function getTime(obj) {
-    //var timeObj = {};
     var deferred = $.Deferred();
 
     var query = "dateTimeJsonFormatter.php";
     $.getJSON(query, function (data) {
         obj.here = data.currentTime.here;
+        obj.here_h = parseInt(data.currentTime.here_h, 10);
+        obj.here_m = parseInt(data.currentTime.here_m, 10);
         obj.there = data.currentTime.there;
+        obj.there_h = parseInt(data.currentTime.there_h, 10);
+        obj.there_m = parseInt(data.currentTime.there_m, 10);
         obj.days_left = data.daysLeft.remaining;
         
+        var date = new Date();
+        var sec = date.getSeconds();
+        obj.here_s = sec;
+        obj.there_s = sec;
+
         deferred.resolve();
     });
     return deferred.promise();
@@ -84,13 +106,84 @@ function printObjects(ctx, weatherObj, timeObj) {
 	
     // There block
 	ctx.fillText("BKK", 50, 350);
-    ctx.fillText(timeObj.here, 450, 390);
+    ctx.fillText(timeFormatter(timeObj, false), 50, 390);
 	ctx.fillText(weatherObj.there_temp+String.fromCharCode(degSign)+"F", 50,
     430);
     
 	// Here block
 	ctx.fillText("LA", 450, 350);
-    ctx.fillText(timeObj.there, 50, 390);
+    ctx.fillText(timeFormatter(timeObj, true), 450, 390);
 	ctx.fillText(weatherObj.here_temp+String.fromCharCode(degSign)+"F", 450,
     430);
+}
+
+// Add leading 0's to hour, minute, seconds
+function intToDoubleDigit(i) {
+    var s = '';
+    if (i < 10) {
+        s += '0' + i;
+    }
+    else {
+        s += i;
+    }
+    return s;
+}
+
+// Format the time
+function timeFormatter(timeObj, here) {
+    var h, m, s;
+    if (here) {
+        h = intToDoubleDigit(timeObj.here_h);
+        m = intToDoubleDigit(timeObj.here_m);
+        s = intToDoubleDigit(timeObj.here_s);
+ 
+    }
+    else {
+        h = intToDoubleDigit(timeObj.there_h);
+        m = intToDoubleDigit(timeObj.there_m);
+        s = intToDoubleDigit(timeObj.there_s);
+    }
+
+    return (h + ':' + m + ':' + s);
+}
+
+// Increase the time by one second or set seconds
+function updateTimeObj(timeObj, seconds) {
+    // Manual set the seconds
+    if (seconds) {
+        timeObj.here_s = seconds;
+        timeObj.there_s = seconds;
+    }
+    // Increment by 1 second
+    else {
+        var hrHere = timeObj.here_h;
+        var hrThere = timeObj.there_h;
+        var min = timeObj.here_m;
+        var sec = timeObj.here_s;
+        sec++;
+        
+        if (sec >= 60) {
+            sec = 0;
+            min++;
+
+            if (min >= 60) {
+                min = 0;
+                hrHere++;
+                hrThere++;
+
+                if (hrHere >= 24 || hrThere >= 24) {
+                    hr = 0;
+                }
+            }
+        }
+        timeObj.here_h = hrHere;
+        timeObj.here_m = min;
+        timeObj.here_s = sec;
+
+        timeObj.there_h = hrThere;
+        timeObj.there_m = min;
+        timeObj.there_s = sec;
+    }
+
+   return timeObj;
 }
